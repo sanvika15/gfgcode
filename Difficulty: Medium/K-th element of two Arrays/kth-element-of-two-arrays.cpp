7c36bfc923c0fa75646646e23fda5192eb4dd781
Sanvika Dakhale <137372400+sanvika15@users.noreply.github.com>
2025-05-16 11:55:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+    int n = a.size(), m = b.size();
+    
+    // Ensure a is the smaller array
+    if (n > m) return kthElement(b, a, k);
+    
+    int low = max(0, k - m), high = min(k, n);
+    
+    while (low <= high) {
+        int cut1 = (low + high) / 2;
+        int cut2 = k - cut1;
+        
+        int l1 = (cut1 == 0) ? INT_MIN : a[cut1 - 1];
+        int l2 = (cut2 == 0) ? INT_MIN : b[cut2 - 1];
+        int r1 = (cut1 == n) ? INT_MAX : a[cut1];
+        int r2 = (cut2 == m) ? INT_MAX : b[cut2];
+        
+        if (l1 <= r2 && l2 <= r1) {
+            return max(l1, l2);
+        } else if (l1 > r2) {
+            high = cut1 - 1;
+        } else {
+            low = cut1 + 1;
+        }
+    }
+    
+    return -1; 
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> a, b;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
