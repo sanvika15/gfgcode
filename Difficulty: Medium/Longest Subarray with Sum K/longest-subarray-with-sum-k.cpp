92aac858ab7ed33c24ca2c71c1a69e43d5dce730
Sanvika Dakhale <137372400+sanvika15@users.noreply.github.com>
2025-05-16 15:14:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        
+        int maxLen = 0;
+        map<long long , int> preSumMap;
+        long long sum = 0;
+        for(int i=0;i<arr.size();i++){
+            sum+= arr[i];
+            if(sum == k) {
+                maxLen = max(maxLen,i+1);
+                
+            }
+            long long rem = sum - k;
+            if(preSumMap.find(rem) != preSumMap.end())
+            
+            {
+              int len = i-preSumMap[rem];
+              maxLen = max(maxLen,len);
+            }
+            
+             if(preSumMap.find(sum) == preSumMap.end())
+             preSumMap[sum] = i;
+        }
+        return maxLen;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore newline character after t
+
+    while (t--) {
+        vector<int> arr;
+        int k;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        cin >> k;
+        cin.ignore(); // Ignore newline character after k input
+
+        Solution solution;
+        cout << solution.longestSubarray(arr, k) << "\n";
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
