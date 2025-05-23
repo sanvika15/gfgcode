26146578e4059a2856243cfdf86536d3943cc256
Sanvika Dakhale <137372400+sanvika15@users.noreply.github.com>
2025-05-23 23:00:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.py
@@ -0,0 +1,13 @@
+class Solution:
+    def startStation(self, gas, cost):
+        if sum(gas) < sum(cost):
+            return -1
+        start_index = 0
+        current_gas = 0
+        
+        for i in range(len(gas)):
+            current_gas += gas[i] - cost[i]
+            if current_gas < 0:
+                start_index = i + 1
+                current_gas = 0
+        return start_index
